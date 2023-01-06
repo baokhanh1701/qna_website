97ee6c39d94cda7f5499ea330d7788fbf4e0ed41
Nguyen Kieu Bao Khanh <khanh.nguyen1701@hcmut.edu.vn>
2023-01-06 11:30:17 +0700

    editing eventService and useLivify

--- src/api/eventService.js
+++ src/api/eventService.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
 export const API_URL = axios.create({
-    baseURL: "https://63b3df865901da0ab3893ffb.mockapi.io/api/v1/eventname",
-})
+    baseURL: "https://63b3df865901da0ab3893ffb.mockapi.io/api/v1",
+});
 
-export const getEvents = async () => {
-    const { data } = await API_URL.get(`/eventname`);
+export const useGetEvents = async () => {
+    const { data } = await API_URL.get(
+        `/eventname`
+    );
+    return data.results;
+}
+
+export const useGetSingleEvent = async (event_id) => {
+    const { data } = await API_URL.get(
+        `/eventname/${event_id}`
+    );
     return data;
 }
--- src/hooks/useLivify.js
+++ src/hooks/useLivify.js
@@ -1,6 +1,21 @@
 import { useEffect, useState } from 'react';
+import { useGetEvents, useGetSingleEvent } from '../api/eventService';
 
-export const useGetEvent = () => {
+export const useGetWholeEvents = () => {
     const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        useGetEvents()
+            .then((results) => setEvents(results))
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    return {
+        events,
+        loading,
+    }
 }
